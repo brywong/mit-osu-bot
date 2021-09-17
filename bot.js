ecdfbe2b2c2a00a5c8b8355ce43ad8a3b6128105
Cory Lynch <cjl2625@gmail.com>
2021-09-17 03:03:08 -0400

    Add test slash command

--- bot.js
+++ bot.js
@@ -1,14 +1,47 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 const config_json_1 = require("./config.json");
+const rest_1 = require("@discordjs/rest");
+const builders_1 = require("@discordjs/builders");
+const v9_1 = require("discord-api-types/v9");
 const client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS, discord_js_1.Intents.FLAGS.GUILD_MESSAGES] });
+const guildId = '661656176244686858';
+const clientId = '888306044558802965';
+const rest = new rest_1.REST({ version: '9' }).setToken(config_json_1.BOT_TOKEN);
+const commands = [
+    new builders_1.SlashCommandBuilder().setName('twig').setDescription("Replies with 'chika'")
+].map(cmd => cmd.toJSON());
+function registerSlashCommands() {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("Registering slash commands");
+        yield rest.put(v9_1.Routes.applicationGuildCommands(clientId, guildId), { body: commands });
+        console.log(`Successfully registered ${commands.length} command(s)`);
+    });
+}
+registerSlashCommands();
 client.once('ready', () => {
     console.log("Bot started");
 });
-client.on('interactionCreate', (interaction) => {
-    console.log(interaction);
-});
+// new slash commands
+client.on('interactionCreate', (interaction) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!interaction.isCommand())
+        return;
+    const { commandName } = interaction;
+    if (commandName === "twig") {
+        yield interaction.reply("chika");
+    }
+}));
+// old-style commands
 client.on('messageCreate', (message) => {
     if (message.content === "!meme") {
         message.reply("Meme!!");
--- bot.ts
+++ bot.ts
@@ -1,15 +1,42 @@
 import { Client, Intents } from "discord.js";
 import { BOT_TOKEN } from "./config.json"
+import { REST } from '@discordjs/rest'
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+import { Routes } from 'discord-api-types/v9'
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+
+const guildId = '661656176244686858';
+const clientId = '888306044558802965';
+const rest = new REST({ version: '9' }).setToken(BOT_TOKEN);
+
+const commands = [
+  new SlashCommandBuilder().setName('twig').setDescription("Replies with 'chika'")
+].map(cmd => cmd.toJSON())
+
+async function registerSlashCommands() {
+  console.log("Registering slash commands");
+
+  await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands });
+  console.log(`Successfully registered ${commands.length} command(s)`);
+}
+
+registerSlashCommands();
+
 
 client.once('ready', () => {
   console.log("Bot started");
 })
 
 // new slash commands
-client.on('interactionCreate', (interaction) => {
-  console.log(interaction);
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+  if (commandName === "twig") {
+    await interaction.reply("chika")
+  }
 })
 
 // old-style commands
