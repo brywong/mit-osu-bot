75e3170f097f49c1df31f6cebb3d16570a523b6b
Cory Lynch <cjl2625@gmail.com>
2021-09-17 02:02:14 -0400

    Add typescript config

---
+++ bot.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log(meme(34));
+function meme(x) {
+    return `${x}`;
+}
---
+++ bot.ts
@@ -0,0 +1,7 @@
+import { Client, Intents } from "discord.js";
+
+console.log(meme(34))
+
+function meme(x: number): string {
+  return `${x}`;
+}
