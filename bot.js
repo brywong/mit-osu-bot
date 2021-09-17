e00df3cd514a19b7b9f580c29da195f645a2d254
Cory Lynch <cjl2625@gmail.com>
2021-09-17 02:47:54 -0400

    Add meme

--- bot.js
+++ bot.js
@@ -2,9 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const discord_js_1 = require("discord.js");
 const config_json_1 = require("./config.json");
-const client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS] });
+const client = new discord_js_1.Client({ intents: [discord_js_1.Intents.FLAGS.GUILDS, discord_js_1.Intents.FLAGS.GUILD_MESSAGES] });
 client.once('ready', () => {
     console.log("Bot started");
 });
-console.log(config_json_1.BOT_TOKEN);
+client.on('interactionCreate', (interaction) => {
+    console.log(interaction);
+});
+client.on('messageCreate', (message) => {
+    if (message.content === "!meme") {
+        message.reply("Meme!!");
+    }
+});
 client.login(config_json_1.BOT_TOKEN);
--- bot.ts
+++ bot.ts
@@ -1,10 +1,22 @@
 import { Client, Intents } from "discord.js";
 import { BOT_TOKEN } from "./config.json"
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 
 client.once('ready', () => {
   console.log("Bot started");
+})
+
+// new slash commands
+client.on('interactionCreate', (interaction) => {
+  console.log(interaction);
+})
+
+// old-style commands
+client.on('messageCreate', (message) => {
+  if (message.content === "!meme") {
+    message.reply("Meme!!");
+  }
 })
 
 client.login(BOT_TOKEN);
